d7df48430357c68132f8bcb66cfb83e02bfd6b41
Hemdeep Saini <hemdeep@zibal.com>
2021-05-25 01:34:45 +0530

    serivice created with a bug

---
+++ src/app/services/post.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { PinModel } from '../notify-by-pin/Pin-model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+
+  private baseUrl="http://127.0.0.1:8000"
+  constructor(private http: HttpClient) { }
+
+  postOrder(data: PinModel): any {
+    return this.http.post<PinModel>(this.baseUrl + '/addOrder', data);
+  }
+}
