4abe84d8f5ba8e22ab75fe5de407f7a1a462f9a2
Hemdeep Saini <hemdeep@zibal.com>
2021-05-25 02:59:17 +0530

    cowin dropdown services added

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -7,10 +7,27 @@ import { PinModel } from '../notify-by-pin/Pin-model';
 })
 export class PostService {
 
-  private baseUrl="http://127.0.0.1:8000"
+  private baseUrl = "http://127.0.0.1:8000";
+  private cowinUrl = "https://cdn-api.co-vin.in/api/v2";
+//  https://cdn-api.co-vin.in/api/v2/admin/location/states
+//  https://cdn-api.co-vin.in/api/v2/admin/location/districts/12
+  
   constructor(private http: HttpClient) { }
 
   postOrder(data: PinModel): any {
     return this.http.post<PinModel>(this.baseUrl + '/addOrder', JSON.stringify(data));
+  }
+
+  getLog(): any{
+    return this.http.get(this.baseUrl + '/getLog');
+  }
+  getPin(): any{
+    return this.http.get(this.baseUrl + '/getPin');
+  }
+  getStates(): any{
+    return this.http.get(this.cowinUrl + '/admin/location/states');
+  }
+  getDistricts(distric_id:string): any{
+      return this.http.get(this.cowinUrl + '/admin/location/districts/'+distric_id);
   }
 }
