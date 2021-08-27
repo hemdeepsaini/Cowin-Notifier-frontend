23fe7f6917f4c58be08a2758dbcf4676e9f70e0f
Hemdeep Saini <hemdeep.saini@gmail.com>
2021-08-27 17:20:59 +0530

    c3

--- server.js
+++ server.js
@@ -1,15 +1,7 @@
-//Install express server
 const express = require('express');
-const path = require('path');
-
 const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/Cowin-Notifier-frontend'));
-
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/Cowin-Notifier-frontend/'}),
-);
-
-// Start the app by listening on the default Heroku port
+app.use(express.static('./dist/cowin-notifier-frontend'));
+app.get('/*', function (req, res) {
+ res.sendFile(__dirname + '/dist/cowin-notifier-frontend/index.html');
+});
 app.listen(process.env.PORT || 8080);
